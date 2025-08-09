94225e4a27be33a27bc31d8fe630a162295e4cf9
himanshu yadav <himanshuyadav1740343@gmail.com>
2025-08-09 21:38:30 +0530

    sdf

---
+++ Chrome-ext/popup.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("myButton").addEventListener("click", clicked);
+});
+
+function clicked() {
+  alert("Button was clicked!");
+}
